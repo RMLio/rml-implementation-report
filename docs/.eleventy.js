9be757df94c4601a85bf72f5066dfcbbe97add91
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2019-07-18 11:43:10 +0200

    reduce size of HTML directly in Eleventy

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -1,6 +1,14 @@
-module.exports = {
-  templateFormats: [
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    if( outputPath.endsWith(".html") ) {
+      return content.replace(/^\s*[\r\n]/gm, '');
+    }
+
+    return content;
+  });
+
+  eleventyConfig.templateFormats = [
     "html",
     "css"
-  ]
+  ];
 };
--- docs/snapshots/reduce-size.sh
+++
@@ -1,8 +0,0 @@
-# $1 path to the file
-
-if [ "$#" -ne 1 ]; then
-    echo "Illegal number of parameters. Provide the path to one file."
-    exit 1
-fi
-
-sed -i '/^ *$/d' $1
