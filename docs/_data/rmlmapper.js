47432f57d24cd95821bdac0f91b68af28d1ae624
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2020-06-09 14:59:09 +0200

    fix #15

--- docs/_data/rmlmapper.js
+++ docs/_data/rmlmapper.js
@@ -1,32 +1,11 @@
-const GraphqlExecutor = require('../graphqlexecutor');
+/**
+ * author: Pieter Heyvaert (pieter.heyvaert@ugent.be)
+ * Ghent University - imec - IDLab
+ */
+
+const TestResultFetcher = require('../test-result-fetcher');
 
 module.exports = async () => {
-    const executor = new GraphqlExecutor();
-    const result = await executor.query('./_data/rmlmapper.nt', {
-    "platform": { "@id": "http://www.w3.org/ns/earl#subject", "@singular": true },
-    "testUri": { "@id": "http://www.w3.org/ns/earl#test", "@singular": true },
-    "result": { "@id": "http://www.w3.org/ns/earl#result", "@singular": true },
-    "outcomeUri": { "@id": "http://www.w3.org/ns/earl#outcome", "@singular": true },
-    "Assertion": "http://www.w3.org/ns/earl#Assertion"
-    }, `{... on Assertion {platform testUri result { outcomeUri }} }`, rmlmapper => {
-        rmlmapper.forEach(result => {
-            result.testName = result.testUri.replace("http://rml.io/test-cases/#","").toUpperCase();
-            result.outcomeUri = result.result.outcomeUri[0];
-            result.outcome = result.outcomeUri.replace("http://www.w3.org/ns/earl#","");
-    });
-
-    rmlmapper.sort((a,b) => {
-        if (a.testName > b.testName) {
-            return 1;
-        } else {
-            return -1;
-        }
-    });
-
-    return rmlmapper;
-});
-
-console.log(result);
-
-return result;
+    const fetcher = new TestResultFetcher();
+    return await fetcher.query('./_data/rmlmapper.nt', 23001);
 };
